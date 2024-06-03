4a5a69fe921a875dc6d5932eaea44af2d7408406
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2024-06-03 22:56:03 +0300

    tel notification

--- js/main.js
+++ js/main.js
@@ -492,9 +492,7 @@ const countries = [
   select_box = document.querySelector(".options"),
   search_box = document.querySelector(".search-box"),
   input_box = document.querySelector('input[type="tel"]'),
-  selected_option = document.querySelector(".selected-option div"),
-  selected_option2 = document.querySelector(".modal-body .selected-option div"),
-  select_box2 = document.querySelector(".modal-body .options");
+  selected_option = document.querySelector(".selected-option div");
 
 let options = null;
 
@@ -509,7 +507,6 @@ for (country of countries) {
   </li> `;
 
   select_box.querySelector("ol").insertAdjacentHTML("beforeend", option);
-  select_box2.querySelector("ol").insertAdjacentHTML("beforeend", option);
   options = document.querySelectorAll(".option");
 }
 
@@ -524,15 +521,10 @@ function selectOption() {
   input_box.value = phone_code.innerText;
 
   select_box.classList.remove("active");
-  select_box2.classList.remove("active");
   selected_option.classList.remove("active");
-  selected_option2.classList.remove("active");
 
   search_box.value = "";
   select_box
-    .querySelectorAll(".hide")
-    .forEach((el) => el.classList.remove("hide"));
-  select_box2
     .querySelectorAll(".hide")
     .forEach((el) => el.classList.remove("hide"));
 }
@@ -551,10 +543,6 @@ function searchCountry() {
 selected_option.addEventListener("click", () => {
   select_box.classList.toggle("active");
   selected_option.classList.toggle("active");
-});
-selected_option2.addEventListener("click", () => {
-  select_box2.classList.toggle("active");
-  selected_option2.classList.toggle("active");
 });
 
 options.forEach((option) => option.addEventListener("click", selectOption));
